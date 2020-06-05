7a61df786bfaf8b6b95fad4738410d7b2c956701
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-06-05 19:12:41 -0300

    Melhora script que executa spiders

--- run.sh
+++ run.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-rm -rf data/output
-mkdir -p data/output data/log
+set -e
+OUTPUT_PATH=data/output
+LOG_PATH=data/log
 
-for spider in pagamento pagamento_historico execucao_despesa orcamento_despesa; do
-	log_filename="data/log/${spider}.log"
-	output_filename="data/output/${spider}.csv.gz"
+run_spider() {
+	spider="$1"
+
+	mkdir -p $LOG_PATH $OUTPUT_PATH
+	log_filename="$LOG_PATH/${spider}.log"
+	output_filename="$OUTPUT_PATH/${spider}.csv.gz"
+	rm -rf $log_filename $output_filename
 	echo "Running ${spider} - check $log_filename for logs and $output_filename for output"
 	time scrapy crawl \
 		--loglevel=INFO --logfile=$log_filename \
 		$spider \
 		-t "csv.gz" \
 		-o $output_filename
-done
+}
+
+if [ ! -z "$1" ]; then
+	run_spider $1
+else
+	for spider in pagamento pagamento_historico execucao_despesa orcamento_despesa; do
+		run_spider $spider
+	done
+fi
