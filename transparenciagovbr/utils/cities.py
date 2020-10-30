ce5d1f6503ded828bf424a88a6883e61d9b6cb91
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2020-10-30 15:14:15 -0300

    Atualiza nome de municípios e população

--- transparenciagovbr/utils/cities.py
+++ transparenciagovbr/utils/cities.py
@@ -2,7 +2,7 @@ import rows
 
 from transparenciagovbr import settings
 
-cities_filename = settings.REPOSITORY_PATH / "data" / "populacao-estimada-2019.csv"
+cities_filename = settings.REPOSITORY_PATH / "data" / "populacao-estimada-2020.csv"
 city_name_by_id = {
     row.city_ibge_code: row.city for row in rows.import_from_csv(cities_filename)
 }
