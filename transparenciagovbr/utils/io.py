0165bf85d292ce5bcf635076fe6ec57d0c3aaede
Álvaro Justen (@turicas) <alvarojusten@gmail.com>
2021-03-02 15:18:43 -0300

    Checagem de nome de arquivo pode ser exp. regular

--- transparenciagovbr/utils/io.py
+++ transparenciagovbr/utils/io.py
@@ -1,3 +1,4 @@
+import re
 from csv import DictReader
 from io import TextIOWrapper
 from zipfile import ZipFile
@@ -16,9 +17,15 @@ class NotNullTextWrapper(TextIOWrapper):
 def parse_zip(filename_or_fobj, inner_filename_suffix, encoding):
     zf = ZipFile(filename_or_fobj)
     for file_info in zf.filelist:
-        if file_info.filename.endswith(inner_filename_suffix):
+        filename = file_info.filename
+        if isinstance(inner_filename_suffix, re.Pattern):
+            file_matches = bool(inner_filename_suffix.findall(filename))
+        else:
+            file_matches = filename.endswith(inner_filename_suffix)
+
+        if file_matches:
             fobj = TextIOWrapper(
-                zf.open(file_info.filename), encoding=encoding
+                zf.open(filename), encoding=encoding
             )
             reader = DictReader(fobj, delimiter=";")
             for row in reader:
